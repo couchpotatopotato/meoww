23672004c931aa8903bd7c5980350074ca49eed6
potato on the couch <nathanielgmh@gmail.com>
2023-02-11 23:11:04 +0800

    chore: remove favourites

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -47,10 +47,6 @@ export default defineComponent({
           name: "Breeds",
           route: "/",
         },
-        {
-          name: "Favorites",
-          route: "/favorites",
-        },
       ],
     };
   },
